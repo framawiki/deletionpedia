ed35d4a3474f947cd05d3e0737bbeeb63f10a869
framawiki <framawiki@tools.wmflabs.org>
2018-05-18 19:18:06 +0200

    Update to use pywikibot

--- antidelete.py
+++ antidelete.py
@@ -14,14 +14,14 @@
 import sys
 import re
 import datetime
-from wikipedia import *
+import pywikibot
 from locale import setlocale, LC_TIME
 
 class Antidelete:
     def __init__(self, lang, patterns):
         self.lang = lang
-        self.frm = Site(lang, 'wikipedia')
-        self.to = Site(lang, 'deleted')
+        self.frm = pywikibot.Site(lang, 'wikipedia')
+        self.to = pywikibot.Site('test2', 'wikipedia')
         self.patterns = patterns
 
     def fetch(self):
@@ -50,7 +50,7 @@ class Antidelete:
 
     def parse_list(self, pagename, title_process = None):
         '''Parse page with list of articles to be deleted'''
-        p = Page(self.frm, pagename) 
+        p = pywikibot.Page(self.frm, pagename) 
         s = p.get()
         re_article = re.compile(self.patterns['regexp'])
         for l in s.splitlines():
@@ -69,7 +69,7 @@ class Antidelete:
         if 'Talk' in title:
             print 'no talk pages yet'
             return
-        page = Page(self.frm, title)
+        page = pywikibot.Page(self.frm, title)
         try:
             article_text = page.get()
         except IsRedirectPage:
@@ -80,7 +80,7 @@ class Antidelete:
             return
 
         if not 'porn' in article_text and not 'xxx' in article_text:
-            dp_page = Page(self.to, title)
+            dp_page = pywikibot.Page(self.to, title)
 
             update_page = False
             try:
@@ -100,40 +100,9 @@ class Antidelete:
 
 
 if __name__ == '__main__':
-
-    patterns = {}
-    patterns['sv'] = {
-        'title': u'Wikipedia:Sidor_föreslagna_för_radering',
-        'regexp': u'{{Wikipedia:Sidor föreslagna för radering/(.*)}}',
-        'test': '{{sffr}}', # not very talkative
-        }
-    patterns['de'] = {
-        'fn_day': lambda d: u'Wikipedia:Löschkandidaten/' + d.strftime('%d. %B %Y'),
-        'locale': 'de_DE',
-        'regexp': '== \[\[(.+)\]\]',
-        'test': u'{{Löschantragstext',
-        }
-    patterns['en'] = {
-        'test': 'Article for deletion',
-        'regexp': '{{Wikipedia:Articles for deletion/(.*)}}',
-        'fn_day': lambda d: 'Wikipedia:Articles_for_deletion/Log/' + d.strftime('%Y_%B_%e'),
-        'fn_title': lambda t: t.replace(' (2nd nomination)', ''),
-        }
-    patterns['fi'] = {
-        'test': 'oistoäänestys}}',
-        'regexp': '{{/(.*)}}',
-        'title': u'Wikipedia:Poistoäänestykset'
-        }
-    patterns['fr'] = {
-        'test': 'uppression}}',
-        'regexp': '[*]{{L[|](.*)}}',
-        'title': u'Wikipédia:Pages_à_supprimer'
-        }
-    patterns['nl'] = {
-        'test': '{{wiu|',
+    ad = Antidelete('test', {
+        'test': '{{delete',
         'regexp': '[*]\[\[(.+)\]\]',
-        'fn_day': lambda d: "Wikipedia:Te beoordelen pagina's/Toegevoegd " + d.strftime('%Y%m%d')
-        }
-    for lang in ['de', 'en', 'fi', 'nl', 'fr', 'sv']:
-        ad = Antidelete(lang, patterns[lang])
-        ad.fetch()
+        'title': 'Test_delete_list'
+        })
+    ad.fetch()
